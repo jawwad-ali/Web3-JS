84103b380b078614299438d8e4fcaadb300f64e8
jawwad-ali <alijawwad001@gmail.com>
2022-10-06 23:43:11 +0500

    added second method to ex_32

--- js/ex_32/app.js
+++ js/ex_32/app.js
@@ -1,14 +1,19 @@
-const allusers = []
 const current_users = ["Virat", "Babar", "Kane", "Smith", "Root"]
-const new_users = ["Shaheen", "Haris", "Babar", "Root", "Shah"]
-
+const new_users = ["Shaheen", "Haris", "Babar", "Root", "Shah"] 
 
 for (var i = 0; i < new_users.length; i++) {
-    // console.log("NEWS_USERS", new_users)
     for (var j = 0; j < current_users.length; j++) {
-        // console.log("CURRENT_USERS", current_users)
         if (new_users[i] === current_users[j]) {
             document.write("Please select a new user name ", new_users[i], " has already been selected. <br />")
         }
     }
 }
+
+// const UserName = prompt("Enter name")
+
+// if (current_users.includes(UserName)) {
+//     document.write("Username already taken")
+// }
+// else {
+//     document.write("Welcome ", UserName)
+// }
